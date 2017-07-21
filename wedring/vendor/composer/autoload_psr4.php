8f9e4d950098db3668086cdeb4f008eb2aaf5cd5
何旭峰 <767056186@qq.com>
2017-07-21 19:53:14 +0800

    春阳解决excel冲突

--- wedring/vendor/composer/autoload_psr4.php
+++ wedring/vendor/composer/autoload_psr4.php
@@ -6,7 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'think\\composer\\' => array($vendorDir . '/topthink/think-installer/src'),
     'think\\captcha\\' => array($vendorDir . '/topthink/think-captcha/src'),
-    'think\\' => array($baseDir . '/thinkphp/library/think'),
 );
